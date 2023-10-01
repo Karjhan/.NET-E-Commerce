a53ddc2571691c0e4eed62d3d6f9c8fe0a05a869
Karjhan <alex_mar1999@yahoo.com>
2023-10-01 23:09:48 +0300

    [alex] added loading interceptor and spinner + ux for home page

--- Frontend Client/client/src/app/shop/product-details/product-details.component.ts
+++ Frontend Client/client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,8 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) {
-    
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', ' ');
   }
 
   ngOnInit(): void {
@@ -23,7 +24,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get("id");
     if (id) {
       this.shopService.getProduct(+id).subscribe({
-        next: response => this.product = response,
+        next: response => {
+          this.product = response;
+          this.bcService.set('@productDetails', response.name)
+        },
         error: error => console.log(error)
       });
     }
